268f13d699781185bda8d8f4a1d07becb94e7ca4
Nitesh <niteshwadhwani605@gmail.com>
2024-01-08 14:20:53 +0530

    worked on app 7minWorkout

---
+++ app/src/main/java/com/example/a7minutesworkout/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.a7minutesworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import com.example.a7minutesworkout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit  var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    binding.flStart.setOnClickListener {
+            Toast.makeText(this,"This is the start of the app",Toast.LENGTH_SHORT).show()
+        val intent = Intent(this,ExerciseActivity::class.java)
+        startActivity(intent)
+    }
+
+    }
+}
