1bd61d4ab63613338f85eac2fc495a6c3cfe6dc4
Nitesh <niteshwadhwani605@gmail.com>
2024-01-10 14:38:29 +0530

    displaying count down of 10 seconds

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -1,24 +1,71 @@
 package com.example.a7minutesworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityExerciseBinding
+    private  var binding: com.example.a7minutesworkout.databinding.ActivityExerciseBinding? = null
+
+   private var restTimer: CountDownTimer? = null
+    private var timerDuration: Long = 10000
+    private var restProgress = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
-        setSupportActionBar(binding.toolbarExercise)
+        setSupportActionBar(binding?.toolbarExercise)
 
         // tool bar to get back to screen when back button is pressed
         if (supportActionBar != null){
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-        binding.toolbarExercise.setNavigationOnClickListener{
+        binding?.toolbarExercise?.setNavigationOnClickListener{
             onBackPressedDispatcher.onBackPressed()
         }
+
+        binding?.tvTimer?.text = (timerDuration/1000).toString()
+
+      setupRestView()
+
+    }
+
+    private fun setupRestView(){
+        if (restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(timerDuration, 1000){
+            override fun onTick(millsUnitFinish: Long) {
+                  restProgress++
+                    binding?.progressBar?.progress = 10 - restProgress
+                    binding?.tvTimer?.text =  (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+               Toast.makeText(this@ExerciseActivity,"Timer is Finished",Toast.LENGTH_SHORT).show()
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+
+        if (restTimer != null){
+           restTimer?.cancel()
+           restProgress = 0
+       }
+        binding = null
+        super.onDestroy()
     }
 }
